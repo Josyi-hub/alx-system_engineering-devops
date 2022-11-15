6a1839e14da44b2168c5237426f55d8bedd097c6
Josyi-hub <yipenenye1@gmail.com>
2022-11-15 01:15:34 -0800

    Add 0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"to-do list information for a given employee ID."""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(f"{url}users/{id}").json()
+    todos = requests.get(f"{url}todos", params={"userId": id}).json()
+    finished = [todo['title'] for todo in todos if todo['completed'] is True]
+    sentence = "is done with task"
+    t = f"Employee {user['name']} {sentence} {len(finished)}/{len(todos)}: "
+    print(t)
+    [print("\t ", todo_title) for todo_title in finished]
