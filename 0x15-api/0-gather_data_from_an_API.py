8b2533e7f65d7e46e49f220e0a4ae45b10ce948f
Josyi-hub <yipenenye1@gmail.com>
2022-11-15 01:56:39 -0800

    Fix documentation problem 5

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Return to-do list information for a given employee ID """
+""" 
+Return to-do list information for a given employee ID 
+"""
 import requests
 import sys
 if __name__ == "__main__":
